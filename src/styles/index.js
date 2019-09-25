395639b4a2d5b74477ffdd775ac84948b4d9c8b7
fanzheng <montage_fz@sina.com>
2019-09-25 20:32:34 +0800

    - [新增] iconfont 字体

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,9 @@
 /**
  * Created by MonTage_fz on 2019/9/25
  */
-
+// 引入字体样式
+import './iconfont/iconfont.css';
+// 引入样式重置
 import './reset.less';
+// 引入 边框1px 解决方案
 import './border.less';
--- src/views/Home/components/Header.vue
+++ src/views/Home/components/Header.vue
@@ -1,14 +1,15 @@
 /**Created by MonTage_fz on 2019/9/25**/
 <template>
-    <div class="Header">
+    <div class="Header bg-color">
         <div class="header-left">
-            返回
+            <i class="iconfont">&#xe6a5;</i>
         </div>
         <div class="header-input">
-            1
+            <i class="iconfont">&#xe607;</i>
+            输入城市/游玩/景点主题
         </div>
         <div class="header-right">
-            城市
+            城市<i class="iconfont">&#xe608;</i>
         </div>
     </div>
 </template>
@@ -28,6 +29,10 @@
 </script>
 
 <style scoped lang="less">
+
+    // 引入 variable css, 定义了全局的样式类
+    @import '~@/styles/variable.less';
+
     // 设计稿给的是750px
     // 我们将html的字体大小设置为75px
     // 那么 1rem = html font-size 75px
@@ -36,21 +41,33 @@
         height: 86px;
         align-items: center;
         display: flex;
-        background: #00bcd4;
+        background: @bgColor;
+
+        .iconfont {
+            font-size: 32px;
+        }
 
         .header-left {
             width: 64px;
+            text-align: center;
         }
 
         .header-input {
             flex: 1;
             background: #ffffff;
             border-radius: 5px;
+            height: 64px;
+            line-height: 64px;
+            margin-left: 20px;
+            color: #ccc;
+            padding-left: 20px;
         }
-
 
         .header-right {
             width: 124px;
+            text-align: center;
+            font-size: 24px;
         }
+
     }
 </style>
