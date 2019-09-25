3ca95756b891c4e97ee444c244b3e6590e19610c
fanzheng <montage_fz@sina.com>
2019-09-25 17:57:11 +0800

    - [新增] 开始编写Header组件

--- src/views/Home/components/Header.vue
+++ src/views/Home/components/Header.vue
@@ -2,13 +2,13 @@
 <template>
     <div class="Header">
         <div class="header-left">
-            hello world
+            返回
         </div>
         <div class="header-input">
-
+            1
         </div>
         <div class="header-right">
-
+            城市
         </div>
     </div>
 </template>
@@ -32,7 +32,25 @@
     // 我们将html的字体大小设置为75px
     // 那么 1rem = html font-size 75px
     .Header {
-        color: red;
-        font-size:35px;
+        color: #fff;
+        height: 86px;
+        align-items: center;
+        display: flex;
+        background: #00bcd4;
+
+        .header-left {
+            width: 64px;
+        }
+
+        .header-input {
+            flex: 1;
+            background: #ffffff;
+            border-radius: 5px;
+        }
+
+
+        .header-right {
+            width: 124px;
+        }
     }
 </style>
