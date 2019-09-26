826ebd2e5b92cff259c8c7ae902f9bbec1f62b8b
fanzheng <montage_fz@sina.com>
2019-09-26 10:18:05 +0800

    - [新增] 完成首页第二个轮波

--- src/views/Home/components/Icons.vue
+++ src/views/Home/components/Icons.vue
@@ -1,34 +1,96 @@
 /**Created by MonTage_fz on 2019/9/25**/
 <template>
     <div class="Icons">
-        <div class="icon"
-             v-for="item of 8">
-            <span>
-            </span>
-            <p>热门景点</p>
-        </div>
+        <Swipe
+            :autoplay="3000"
+            indicator-color="white"
+        >
+            <SwipeItem
+                v-for="(itemList,index) of pages"
+                :key="index"
+            >
+                <div class="icon"
+                     v-for="item of itemList">
+                    <span></span>
+                    <p>{{item.title}}</p>
+                </div>
+            </SwipeItem>
+        </Swipe>
     </div>
 </template>
 
 <script>
+    import {Swipe, SwipeItem} from 'vant';
+
     export default {
         name: 'Icons',
-        components: {},
+        components: {
+            Swipe,
+            SwipeItem,
+        },
         data() {
-            return {};
+            return {
+                iconList: [
+                    {
+                        title: '签证·wifi',
+                        class: '',
+                    }, {
+                        title: '出境游',
+                        class: '',
+                    }, {
+                        title: '一日游',
+                        class: '',
+                    }, {
+                        title: '美食林',
+                        class: '',
+                    }, {
+                        title: '汽车票',
+                        class: '',
+                    }, {
+                        title: '邮轮游',
+                        class: '',
+                    }, {
+                        title: '超级巴士',
+                        class: '',
+                    }, {
+                        title: '攻略',
+                        class: '',
+                    }, {
+                        title: '第二页',
+                        class: '',
+                    },
+                ],
+            };
         },
         created() {
         },
-        computed: {},
+        computed: {
+            pages() {
+                return this.iconList.reduce((p, i, index) => {
+                    let page = Math.floor(index / 8);
+                    if (!Array.isArray(p[page])) {
+                        p[page] = [];
+                    }
+                    p[page].push(i);
+                    return p;
+                }, []);
+            },
+
+        },
         methods: {},
     };
 </script>
 
 <style scoped lang="less">
     .Icons {
-        display: flex;
-        flex-wrap: wrap-reverse;
         height: 316px;
+        width: 100%;
+
+        .van-swipe-item {
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap-reverse;
+        }
 
         .icon {
             float: left;
