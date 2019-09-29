f12e50c64f59af5271d91f665e5c8b80c99856ab
fanzheng <montage_fz@sina.com>
2019-09-29 16:52:14 +0800

    - [新增] 因为滚动条受到全局的影响, 所以我们使用scrollBehavior路由钩子, 解决初始化问题

---
+++ src/components/common/Fade/index.vue
@@ -0,0 +1,25 @@
+/**Created by montage_fz on 2019-09-29**/
+<template>
+    <div class="Fade">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Fade',
+        components: {},
+        data() {
+            return {};
+        },
+        created() {
+        },
+        computed: {},
+        methods: {},
+    };
+</script>
+
+<style scoped lang="less">
+    .Fade {
+    }
+</style>
